e36f7ad2740e8f442f6b03c716d0ba2935b22443
SteamyBuns <gariks1211@gmail.com>
2022-04-20 18:23:02 -0700

    Part 5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
+// const generateImage = require("./generateImage")
+
 const client = new Discord.Client({
     intents: [
         "GUILDS",
@@ -10,57 +12,47 @@ const client = new Discord.Client({
 })
 
 let bot = {
-    client,
+    client, 
     prefix: "$",
     owners: ["529394825024372756"]
 }
 
 client.commands = new Discord.Collection()
 client.events = new Discord.Collection()
+client.slashcommands = new Discord.Collection()
+client.buttons = new Discord.Collection()
 
 client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
 client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload)
+client.loadSlashCommands = (bot, reload) => require("./handlers/slashcommands")(bot, reload)
+client.loadButtons = (bot, reload) => require("./handlers/buttons")(bot, reload)
 
 client.loadEvents(bot, false)
 client.loadCommands(bot, false)
+client.loadSlashCommands(bot, false)
+client.loadButtons(bot, false)
+
 
 module.exports = bot
-
 
 // client.on("ready", () => {
 //     console.log(`Logged in as ${client.user.tag}`)
 // })
 
 // client.on("messageCreate", (message) => {
-//     if (message.content == "hi") {
+//     if (message.content == "hi"){
 //         message.reply("Hello World!")
 //     }
 // })
 
-// const welcomeChannelId = "870423038024384542"
+// const welcomeChannelId = "926530810008453120"
 
-// client.on("guildMemberAdd", (member) => {
-//     member.guild.channels.cache.get(welcomeChannelId).send(`<@${member.id}> Welcome to the server!`)
+// client.on("guildMemberAdd", async (member) => {
+//     const img = await generateImage(member)
+//     member.guild.channels.cache.get(welcomeChannelId).send({
+//         content: `<@${member.id}> Welcome to the server!`,
+//         files: [img]
+//     })
 // })
-
-
-client.slashcommands = new Discord.Collection()
-
-client.loadSlashCommands = (bot, reload) => require("./handlers/slashcommands")(bot, reload)
-client.loadSlashCommands(bot, false)
-
-client.on("interactionCreate", (interaction) => {
-    if (!interaction.isCommand()) return
-    if (!interaction.inGuild()) return interaction.reply("This command can only be used in a server")
-
-    const slashcmd = client.slashcommands.get(interaction.commandName)
-
-    if (!slashcmd) return interaction.reply("Invalid slash command")
-
-    if (slashcmd.perms && !interaction.member.permissions.has(slashcmd.perm))
-    return interaction.reply("You do not have permission for this command")
-
-    slashcmd.run(client, interaction)
-})
 
 client.login(process.env.TOKEN)
